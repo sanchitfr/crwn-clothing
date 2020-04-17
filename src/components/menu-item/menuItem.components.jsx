b06f17a63bf9178b00ca7ac569a7719b6682ebbb
Sanchit <sanchit522@gmail.com>
2020-04-17 21:40:50 +0530

    broke homepage into different components and styled menu items

---
+++ src/components/menu-item/menuItem.components.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './menuItem.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className= {`menu-item ${size}`}>
+        <div  style={{
+        backgroundImage : `url(${imageUrl})`
+        }}
+         className='backgroundImage'
+         >
+        </div>
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
